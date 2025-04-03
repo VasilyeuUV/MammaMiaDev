39c32a238dd8dcfa8d75385dedf5b8053f489e53
VasilyeuUV <vasilyeu.uv@mail.ru>
2025-04-04 00:13:38 +0300

    Добавьте файлы проекта.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MammaMiaDev.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
