132b9dcaf04852f5d07f44948383d85da407253e
VasilyeuUV <vasilyeu.uv@mail.ru>
2025-04-04 21:27:12 +0300

    Avalonia UI - 03 - Sidebar Menu

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,44 @@
-﻿namespace MammaMiaDev.ViewModels
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MammaMiaDev.Templates;
+
+namespace MammaMiaDev.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private bool _isPaneOpen = true;
+    [ObservableProperty] private ViewModelBase? _currentPage = new HomePageViewModel();
+    [ObservableProperty] private ListItemTemplate? _selectedListItem;
+
+    public ObservableCollection<ListItemTemplate> Items { get; } 
+        = [
+            new ListItemTemplate(typeof(HomePageViewModel), "HomeRegular"),
+            new ListItemTemplate(typeof(ButtonPageViewModel), "CursorHoverRegular")
+          ];
+
+
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        if (value is null)
+        {
+            return;
+        }
+
+        var instance = Activator.CreateInstance(value.ModelType);
+        if (instance is null)
+        {
+            return;
+        }
+
+        CurrentPage = (ViewModelBase)instance;
     }
+
+
+    [RelayCommand]
+    private void TriggerPane()
+        => IsPaneOpen = !IsPaneOpen;
 }
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,8 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace MammaMiaDev.ViewModels
+namespace MammaMiaDev.ViewModels;
+
+public class ViewModelBase : ObservableObject
 {
-    public class ViewModelBase : ObservableObject
-    {
-    }
 }
