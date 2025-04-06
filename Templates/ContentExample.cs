8c7af5b47654eb141dce61fc2572e9a3eebfa910
VasilyeuUV <vasilyeu.uv@mail.ru>
2025-04-07 01:04:09 +0300

    Правки.

--- Templates/ContentExample.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MammaMiaDev.Templates;
-
-/// <summary>
-/// Шаблон, который позволяет каждому представлению иметь более менее одинаковый вид.
-/// </summary>
-public class ContentExample
-{
-}
